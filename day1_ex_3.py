89ecc0dc538cfa346e6dc083e067cc830166eddc
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-04-04 19:28:24 +0530

    Create day1_ex_3.py

---
+++ day1_ex_3.py
@@ -0,0 +1,10 @@
+#OOPR-Exer-3
+#Start writing your code here
+class Employee:
+    def __init__(self):
+        self.name=None
+        self.age=None
+        self.salary=None
+
+Jack = Employee()
+Jill = Employee()
