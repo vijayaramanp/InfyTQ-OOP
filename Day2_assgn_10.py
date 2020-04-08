f06b857291297b0997dba63394cac941ed304208
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-04-08 18:35:49 +0530

    Update Day2_assgn_10.py

--- Day2_assgn_10.py
+++ Day2_assgn_10.py
@@ -1,7 +1,7 @@
 #OOPR-Assgn-10
 
 class CallDetail:
-    def __init__(self,phoneno,called_no,duration,call_type):
+     def __init__(self,phoneno,called_no,duration,call_type):
         self.__phoneno=phoneno 
         self.__called_no=called_no
         self.__duration=duration 
@@ -11,18 +11,16 @@ class Util:
     def __init__(self):
         self.list_of_call_objects=None
 
-    def parse_customer(self,list_of_call_string): 
-        self.list_of_call_objects=[] 
+    def parse_customer(self,list_of_call_string):
+        list_of_call=[] 
         
         for calldetail in list_of_call_string: 
             tmp=calldetail.split(',')
-            obj=CallDetail(int(tmp[0]),int(tmp[1]),int(tmp[2]),tmp[3])
-            self.list_of_call_objects.append(obj)
-            
-            
-           
-        #self.list_of_call_objects=list(list_of_call_string)
-        #print(self.list_of_call_objects)
+            obj=CallDetail(tmp[0],tmp[1],tmp[2],tmp[3])
+            list_of_call.append(obj)
+        self.list_of_call_objects=list_of_call
+        return self.list_of_call_objects
+        
 
 call='9990000001,9330000001,23,STD'
 call2='9990000001,9330000002,54,Local'
