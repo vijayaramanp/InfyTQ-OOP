b8aab999ad60636fdf1cce278e26eeb8f65aeeb7
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-04-11 12:42:16 +0530

    Update day2_ex_5_6.py

--- day2_ex_5_6.py
+++ day2_ex_5_6.py
@@ -1,3 +1,58 @@
+#Code 1
+===================================================================================
+#OOPR-Exer-6
+#Start writing your code here
+class Vehicle:
+    def __init__(self):
+        self.mileage=None
+        self.fuel_left=None
+
+    def identify_distance_travelled(self,initial_fuel):
+        distance_travelled=(initial_fuel-self.fuel_left)*self.mileage
+        return distance_travelled
+        
+    def identify_distance_that_can_be_travelled(self):
+        if self.fuel_left > 5:
+            return (self.fuel_left-5)*self.mileage
+            
+        else:
+            return 0
+        
+v1=Vehicle()
+v1.mileage=10
+v1.fuel_left=10
+initial_fuel=20
+print(v1.identify_distance_travelled(initial_fuel))
+print(v1.identify_distance_that_can_be_travelled())
+==============================================================================
+
+#Code 2
+===============================================================================
+#OOPR-Exer-6
+#Start writing your code here
+class Vehicle:
+    def __init__(self):
+        self.mileage=None
+        self.fuel_left=None
+
+    def identify_distance_travelled(self,initial_fuel):
+        distance_travelled=(initial_fuel-self.fuel_left)*self.mileage
+        return distance_travelled
+        
+    def identify_distance_that_can_be_travelled(self):
+        if self.fuel_left > 5:
+            return (self.fuel_left-5)*self.mileage
+            
+        else:
+            return 0
+        
+v1=Vehicle()
+v1.mileage=10
+v1.fuel_left=10
+initial_fuel=20
+print(v1.identify_distance_travelled(initial_fuel))
+print(v1.identify_distance_that_can_be_travelled())
+
 #OOPR-Exer-6
 #Start writing your code here
 class Vehicle:
