ae0855740a5cb6e4cea6c965b333c8b09285f68f
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-04-16 10:46:10 +0530

    Create day3_assgn_20_job_application.py

---
+++ day3_assgn_20_job_application.py
@@ -0,0 +1,35 @@
+#OOPR-Assgn-20
+#Start writing your code here
+class Applicant:
+    __application_dict={"A":4,"B":3,"C":5}
+    __applicant_id_count=1000
+    
+    def __init__(self,applicant_name):
+        self.__applicant_id=None
+        self.__applicant_name=applicant_name
+        self.__job_band=None
+    @staticmethod
+    def get_application_dict():
+        return Applicant.__application_dict
+        
+    def get_applicant_name(self):
+        return self.__applicant_name
+    def get_applicant_id(self):
+        return self.__applicant_id
+    def get_job_band(self):
+        return self.__job_band
+        
+    def generate_applicant_id(self):
+        Applicant.__applicant_id_count+=1 
+        self.__applicant_id=Applicant.__applicant_id_count
+    
+    def apply_for_job(self,job_band):
+        if Applicant.__application_dict[job_band]<5:
+            Applicant.__application_dict[job_band]+=1
+            self.generate_applicant_id()
+            self.__job_band=job_band
+        else:
+            return -1
+
+a1=Applicant("Vetri")
+a1.apply_for_job("A")
