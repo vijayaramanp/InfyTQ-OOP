16aeb2a119f05224d9cf5dcc57223ce0fb405954
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-05-02 18:13:21 +0530

    Update day4_assgn_25.py

--- day4_assgn_25.py
+++ day4_assgn_25.py
@@ -32,13 +32,11 @@ class Purchase:
     __counter=101
     
     def __init__(self,customer,fruit_name,quantity):
-        self.__purchase_id='P'+str(Purchase.__counter)
+        self.__purchase_id=None
         self.__customer=customer
         self.__fruit_name=fruit_name
         self.__quantity=quantity
-        Purchase.__counter+=1 
         
-    
     def get_purchase_id(self):
         return self.__purchase_id
     def get_customer(self):
@@ -47,34 +45,20 @@ class Purchase:
         return self.__quantity
         
     def calculate_price(self):
-        fruit_name_list=FruitInfo.get_fruit_name_list()
         max_price=max(FruitInfo.get_fruit_price_list())
         min_price=min(FruitInfo.get_fruit_price_list())
         
-        if self.__fruit_name in fruit_name_list:
-            price_kg=FruitInfo.get_fruit_price(self.__fruit_name)
+        price_kg=FruitInfo.get_fruit_price(self.__fruit_name)
+        if price_kg>0:
+            self.__purchase_id='P'+str(Purchase.__counter)
+            Purchase.__counter+=1
             bill_amount=self.__quantity*price_kg
-            if self.__customer.get_cust_type()=="wholesale":
-                if price_kg==max_price:
-                    if self.__quantity>1:
-                        bill_amount=bill_amount - bill_amount*0.02
-                        bill_amount=bill_amount - bill_amount*0.10
-                    else:
-                        bill_amount=bill_amount - bill_amount*0.10 
-                        
-                elif price_kg==min_price:
-                    if self.quantity>4:
-                        bill_amount=bill_amount - bill_amount*0.05
-                        bill_amount=bill_amount - bill_amount*0.10
-                    else:
-                        bill_amount=bill_amount - bill_amount*0.10
-                else:
-                    bill_amount=bill_amount - bill_amount*0.10
-                    
-            elif self.__customer.get_cust_type()=="Retailer":
-                if price_kg==max_price and self.__quantity>1:
+            if price_kg==max_price and self.__quantity>1:
                     bill_amount=bill_amount - bill_amount*0.02
-                elif price_kg==min_price and self.__quantity>4:
+            elif price_kg==min_price and self.__quantity>4:
+                    bill_amount=bill_amount - bill_amount*0.05   
+                    
+            if Customer.get_cust_type(self.__customer)=="wholesale":
                     bill_amount=bill_amount - bill_amount*0.10
                     
             return bill_amount
